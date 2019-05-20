507a258642673d63fd15f98c0495b8ce899065b5
Gregory James <gregoryjms7@gmail.com>
2019-05-20 09:46:32 -0700

    Reformat code complete

--- guessing_game.py
+++ guessing_game.py
@@ -1,74 +1,49 @@
 import random
+import pyfiglet
 
-"""
- Errors that need to be fixed
 
-- Run Script
-
- Program crashes at any point due to an uncaught exception.
-
-Reviewer Comments:
-
-Putting in a nonnumber crashes the program.
-
-- User input
-
- The player was not prompted for their number choice.
-
-- Displaying Tries
-
- The player was not shown how many attempts they made at the end of each game.
-
-Reviewer Comments:
-
-You do show the number of tries but you are not resetting that number to 0 
-in between games. So, after playing the game, the next game I start at whatever 
-number of tries I ended the last game on. 
- """
 def start_game():
-    # Welcome player
-    print("--------------------------------\n"
-          "Welcome to the Number Guessing Game\n"
-          "--------------------------------\n")
-    # Variables for while loop
+    # welcome player
+    name = input("Hello please enter your name to start the game (or 'quit' to quit): ")
+    if name == 'quit':
+        quit()
+    else:
+        welcome = pyfiglet.figlet_format("welcome!! {}".format(name.title()), font='cyberlarge')
+        saw = pyfiglet.figlet_format("Lets Play a Number Guessing Game")
+        print(welcome)
+        print(saw)
+
     atmpts = 0
-    answer = int(4)
-    # answer = int(random.randrange(1, 10))
-    # continuously prompt player for a guess
+    answer = random.randint(0, 9)
     while True:
-        # Try block for user error
+        atmpts += 1
         try:
-            # Body of while loop
             guess = int(input("Pick a number between 1-10: "))
 
-            atmpts += 1
-            if guess > answer:
-                    print("It's lower")
-            elif guess < answer:
-                    print("It's Higher")
-            else:
+            if guess == answer:
                 print("You did it!!")
                 print("It took you {} attempts".format(atmpts))
                 game_replay = input("Would you like to play again [y]es/[n]o?")
+
                 if game_replay == 'y':
-                    # show lowest number for score
-                    score = []
-                    score += [atmpts]
-                    # make HIGHSCORE static number that show the lowest number
-                    print("\n\nThe HIGHSCORE is {}".format(score))
+                    atmpts = 0
                     continue
+
                 else:
+                    print("You did great. See you later.")
                     break
+
+            elif guess > answer:
+                print("It's lower")
+            elif guess < answer:
+                print("It's Higher")
         except ValueError:
+            # err does not give the item that caused the err
             print("Please enter a number between 1-10.")
         else:
             continue
-
-# store highscroe in list
-# print highest number
 
 
 if __name__ == '__main__':
     # Kick off the program by calling the start_game function.
     start_game()
-
